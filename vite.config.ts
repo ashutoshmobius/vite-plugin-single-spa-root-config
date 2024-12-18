b25f611b8a0e444cbfc3454566b99e74d846d58d
ashutosh <ashutosh.k@mobiusdtaas.ai>
2024-12-18 08:29:10 +0530

    init setup

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+// import { defineConfig } from 'vite'
+
+// export default defineConfig({
+//   plugins: [],
+// })
+
+import { defineConfig } from 'vite';
+import vitePluginSingleSpa from 'vite-plugin-single-spa';
+
+// https://vite.dev/config/
+
+export default defineConfig({
+  plugins: [
+    vitePluginSingleSpa({
+      type: 'root',
+      // importMaps: {
+      //   type: 'systemjs-importmap', // SystemJS import map
+      //   dev: 'src/importMap.dev.json', // Development import map
+      //   build: 'src/importMap.json', // Production import map
+      // },
+      // imo: true, // Enable import-map-overrides functionality
+      // imoUi: {
+      //   variant: 'popup', // Show as popup UI
+      //   buttonPos: 'top-right', // Position override UI at the top-right
+      //   localStorageKey: 'my-custom-imo-ui-key', // Optional: Custom key for toggling the UI
+      // },
+    }),
+  ],
+  server: {
+    port: 9000, // Root-config runs on port 9000
+  },
+  build: {
+    target: 'esnext',
+    minify: false,
+  },
+});
+
+
